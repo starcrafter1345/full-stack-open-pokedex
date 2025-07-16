1ab80944ddb2c093db2b870a2b5ac85122e45b62
starcrafter1345 <starcrafter1345@gmail.com>
2025-07-16 17:10:09 +0200

    added endpoint

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static('dist'))
 
 app.get('/health', (req, res) => res.send('ok'))
 
+app.get('/version', (req, res) => res.send('1'))
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
